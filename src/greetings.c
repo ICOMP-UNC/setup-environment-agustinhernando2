9ddb31a39f5e8914e2decdcfaf456e602417f4d7
Agustin Hernando <alex.hernando@mi.unc.edu.ar>
2024-04-02 11:29:48 -0300

    CL:
    Add executables configurations

---
+++ src/greetings.c
@@ -0,0 +1,7 @@
+#include "greetings.h"
+#include <stdio.h>
+
+void greetings()
+{
+    printf("hello\n");
+}
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "greetings.h"
+
+int main(int argc, char const *argv[])
+{
+    greetings();
+    return 0;
+}
